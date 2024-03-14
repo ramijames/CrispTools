5e2652ab08bca55b7ab318fd86c47d631b849ba7
Rami James <ramijames@gmail.com>
2024-03-14 10:22:18 -0400

    Code formatting, tabs

--- src/components/FluidType.vue
+++ src/components/FluidType.vue
@@ -1,39 +1,42 @@
 <script>
 import { ref, computed, watch } from "vue";
 import CrispInput from "./CrispInput.vue";
+import Button from "./Button.vue";
+import {Tabs, Tab} from 'vue3-tabs-component';
 
 export default {
   data() {
     return {
-      typescales: {
-        MinorSecond: { value: 1.067, label: "Minor Second", selected: false },
-        MajorSecond: { value: 1.125, label: "Major Second", selected: false },
-        MinorThird: { value: 1.2, label: "Minor Third", selected: true },
-        MajorThird: { value: 1.25, label: "Major Third", selected: false },
-        PerfectFourth: {
-          value: 1.333,
-          label: "Perfect Fourth",
-          selected: false,
-        },
-        AugmentedFourth: {
-          value: 1.414,
-          label: "Augmented Fourth",
-          selected: false,
-        },
-        PerfectFifth: { value: 1.5, label: "Perfect Fifth", selected: false },
-        GoldenRatio: { value: 1.618, label: "Golden Ratio", selected: false },
-      },
+      // typescales: {
+      //   MinorSecond: { value: 1.067, label: "Minor Second", selected: false },
+      //   MajorSecond: { value: 1.125, label: "Major Second", selected: false },
+      //   MinorThird: { value: 1.2, label: "Minor Third", selected: true },
+      //   MajorThird: { value: 1.25, label: "Major Third", selected: false },
+      //   PerfectFourth: {
+      //     value: 1.333,
+      //     label: "Perfect Fourth",
+      //     selected: false,
+      //   },
+      //   AugmentedFourth: {
+      //     value: 1.414,
+      //     label: "Augmented Fourth",
+      //     selected: false,
+      //   },
+      //   PerfectFifth: { value: 1.5, label: "Perfect Fifth", selected: false },
+      //   GoldenRatio: { value: 1.618, label: "Golden Ratio", selected: false },
+      // },
       units: {
         px: { label: "px", selected: true },
         rem: { label: "rem", selected: false },
       },
       // baseSize: 18,
-      selectedTypeScale: "MinorThird",
+      // selectedTypeScale: "MinorThird",
     };
   },
   setup() {
     const baseSize = ref(18);
     const selectedUnit = ref("px");
+    const selectedTypeScale =  ref("MinorThird");
     let previousUnit = ref("px");
 
     watch(selectedUnit, (newUnit, oldUnit) => {
@@ -64,14 +67,30 @@ export default {
       return type;
     };
 
+    const typescales = {
+      MinorSecond: { value: 1.067, label: "Minor Second", selected: false },
+      MajorSecond: { value: 1.125, label: "Major Second", selected: false },
+      MinorThird: { value: 1.2, label: "Minor Third", selected: true },
+      MajorThird: { value: 1.25, label: "Major Third", selected: false },
+      PerfectFourth: { value: 1.333, label: "Perfect Fourth", selected: false,},
+      AugmentedFourth: { value: 1.414, label: "Augmented Fourth", selected: false,},
+      PerfectFifth: { value: 1.5, label: "Perfect Fifth", selected: false },
+      GoldenRatio: { value: 1.618, label: "Golden Ratio", selected: false },
+    };
+
     return {
       typeSizes,
       baseSize,
       selectedUnit,
+      typescales,
+      selectedTypeScale
     };
   },
   components: {
     CrispInput,
+    Button,
+    Tabs,
+    Tab,
   },
   methods: {
     copyToClipboard() {
@@ -99,7 +118,9 @@ export default {
       </section>
       <section class="flex p-4 gap-2 w-full border-b b-slate-200">
         <div class="mb-4 w-1/3">
-          <label class="block mb-1 text-sm" for="input1">Scale Ratio</label>
+          <label class="block mb-1 font-semibold text-sm" for="input1"
+            >Scale Ratio</label
+          >
           <select
             v-model="selectedTypeScale"
             class="block appearance-none w-full bg-white border hover:border-blue-500 px-4 py-2 pr-8 rounded focus:outline-none focus:shadow-outline"
@@ -118,7 +139,9 @@ export default {
           </select>
         </div>
         <div class="mb-4 w-1/3">
-          <label class="block mb-1 text-sm" for="input1">Unit</label>
+          <label class="block mb-1 font-semibold text-sm" for="input1"
+            >Unit</label
+          >
           <select
             v-model="selectedUnit"
             class="block appearance-none w-full bg-white border hover:border-blue-500 px-4 py-2 pr-8 rounded focus:outline-none focus:shadow-outline"
@@ -135,7 +158,7 @@ export default {
           </select>
         </div>
         <div class="mb-4 w-1/3">
-          <label class="block mb-1 text-sm" for="input1"
+          <label class="block mb-1 font-semibold text-sm" for="input1"
             >Basesize in {{ selectedUnit }}</label
           >
           <CrispInput v-model="baseSize" type="number" inputType="secondary" />
@@ -167,45 +190,111 @@ export default {
             </li>
           </ul>
         </section>
-        <section id="cssoutput" class="w-full">
-          <pre>
-            <code>
+        <section id="cssoutput" class="w-full border-t my-8 b-slate-200">
+          <tabs :options="{ useUrlFragment: false }" @clicked="tabClicked" @changed="tabChanged" nav-item-class="nav-item">
+            <tab name="Plain CSS" >
+              <div class="mx-auto max-w-7xl bg-gray-900 shadow-2xl sm:rounded-3xl sm:p-16">
+                <pre v-highlightjs>
+                  <code class="css">html {font-size: 16px;}
 
-                .h1 {
-                  font-size: {{ typeSizes(typescales[selectedTypeScale].value, baseSize).h1 }}{{ selectedUnit }};
-                }
-                .h2 {
-                  font-size: {{ typeSizes(typescales[selectedTypeScale].value, baseSize).h2 }}{{ selectedUnit }};
-                }
-                .h3 {
-                  font-size: {{ typeSizes(typescales[selectedTypeScale].value, baseSize).h3 }}{{ selectedUnit }};
-                }
-                .h4 {
-                  font-size: {{ typeSizes(typescales[selectedTypeScale].value, baseSize).h4 }}{{ selectedUnit }};
-                }
-                .h5 {
-                  font-size: {{ typeSizes(typescales[selectedTypeScale].value, baseSize).h5 }}{{ selectedUnit }};
-                }
-                .p {
-                  font-size: {{ typeSizes(typescales[selectedTypeScale].value, baseSize).p }}{{ selectedUnit }};
-                }
-                .small {
-                  font-size: {{ typeSizes(typescales[selectedTypeScale].value, baseSize).small }}{{ selectedUnit }};
-                }
+body {
+  font-family: sans-serif;
+  font-weight: false;
+  line-height: 1.65;
+}
 
-            </code>
-          </pre>
+p {
+  font-size: 1em;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5 {
+  font-family: sans-serif;
+  font-weight: true;
+  line-height: 1.15;
+}
+
+h1,
+.text-size-h1 {
+  font-family: sans-serif;
+  font-weight: 700;
+  margin-top: 0;
+  font-size: 3.05em;
+}
+
+h2,
+.text-size-h2 { font-size: 2.44em; }
+
+h3,
+.text-size-h3 { font-size: 1.95em; }
+
+h4,
+.text-size-h4 { font-size: 1.56em; }
+
+h5,
+.text-size-h5 { font-size: 1.25em; }
+
+.text-caption { font-size: 0.8em; }
+
+small,
+.text-small {
+  font-size: 0.64em;
+}
+{{ generateCssOutput }}
+                </code>
+              </pre>
+            </div>
+            </tab>
+            <tab name="CSS Variables">
+              <p>This is the content of Tab 2</p>
+            </tab>
+            <tab name="SASS Variables">
+              <p>This is the content of Tab 3</p>
+            </tab>
+            <tab name="Tailwind">
+              <p>This is the content of Tab 3</p>
+            </tab>
+          </tabs>
         </section>
       </section>
     </section>
   </div>
 </template>
 
-<style scoped>
-#toolbar {
-  justify-content: space-between;
-  border-left: 0;
-  border-top: 0;
-  border-right: 0;
+<style>
+.css.hljs {
+  background: transparent;
 }
+
+.tabs-component-tabs {
+  display: flex;
+  flex-direction: row;
+  gap: 1rem;
+  margin: 2rem 1rem;
+}
+
+.nav-item {
+  padding: .5rem;
+  border-radius: .35rem;
+  border: 1px solid rgb(235, 235, 235);
+}
+
+.nav-item:hover {
+  border: 1px solid rgb(59, 130, 246)
+}
+
+.is-active {
+  background-color: rgb(37, 99, 235);
+  border: 1px solid rgb(37, 99, 235);
+  color:white;
+}
+
+.is-inactive {
+  background-color: rgb(245, 245, 245);
+  color:rgb(37, 99, 235);
+}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import "./assets/main.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import VueHighlightJS from 'vue3-highlightjs'
+import 'highlight.js/styles/atom-one-dark.css'
 
 const app = createApp(App);
 
 app.use(router);
-
+app.use(VueHighlightJS);
 app.mount("#app");
