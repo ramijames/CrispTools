0ca6245854cacba5d1783c8f37d64d6e756aae34
Rami James <ramijames@gmail.com>
2024-03-16 16:23:38 -0400

    Basic keyword extraction, list creation, limit to 10 shown initially

--- src/components/WordCounter.vue
+++ src/components/WordCounter.vue
@@ -1,6 +1,7 @@
 <script>
 import { ref, computed, watch } from "vue";
 import Button from './shared/Button.vue';
+import keywordExtractor from 'keyword-extractor';
 
 export default {
   data() {
@@ -9,6 +10,7 @@ export default {
     };
   },
   setup() {
+    // Basic Word Counter
     const text = ref('');
     const wordCount = ref(0);
     const sentenceCount = ref(0);
@@ -26,19 +28,56 @@ export default {
 
     watch(text, analyzeText);
 
+    // Keyword Extractor
+    const keywords = ref([]);
+    const keywordCounts = ref(0);
+    const limit = ref(10); // initial limit
+
+    const showMore = () => {
+      limit.value += 5;
+    };
+
+    watch(text, () => {
+      const extractionResult = keywordExtractor.extract(text.value, {
+        language: "english",
+        remove_digits: true,
+        return_changed_case: true,
+        remove_duplicates: false
+      });
+
+      let keywordCounts = extractionResult.reduce((counts, keyword) => {
+        if (counts[keyword]) {
+          counts[keyword]++;
+        } else {
+          counts[keyword] = 1;
+        }
+        return counts;
+      }, {});
+
+      const entries = Object.entries(keywordCounts);
+      const sortedEntries = entries.sort((a, b) => b[1] - a[1]);
+      const filteredEntries = sortedEntries.filter(([keyword, count]) => count >= 3);
+      keywordCounts = Object.fromEntries(filteredEntries);
+
+      keywords.value = keywordCounts;
+    });
+
     return {
       wordCount,
       sentenceCount,
       paragraphCount,
-      text
+      text,
+      keywords,
+      limit,
+      showMore
     };
 
   },
   components: {
     Button,
+
   },
   methods: {
-
     
   },
   watch: {
@@ -65,14 +104,14 @@ export default {
       <section class="flex p-4 gap-2 border-b b-slate-200">
         
       </section>
-      <section class="flex flex-row items-center justify-center my-6 w-full">
-            <section class="counter w-1/2 h-96 bg-yellow-50 p-6">
+      <section class="flex flex-row items-center justify-top my-6 w-full">
+            <section class="counter w-1/2 min-h-96 bg-yellow-50 p-6">
               <div class="font-bold text-xl mb-2">Paste your text</div>
               <p class="text-gray-700 text-base">
                 <textarea v-model="text" id="wordCounterBox" rows="10" class="block appearance-none w-full bg-white border hover:border-blue-500 px-4 py-2 pr-8 rounded focus:outline-none focus:shadow-outline">Paste your text here</textarea>
               </p>
             </section>
-            <section class="analysis w-1/2 h-96 bg-neutral-100 p-6">
+            <section class="analysis w-1/2 min-h-96 bg-neutral-100 p-6">
               <div class="font-bold text-xl mb-2">Analysis</div>
               <div class="panel">
                 <ul>
@@ -81,6 +120,11 @@ export default {
                   <li>Paragraphs: {{ paragraphCount }}</li>
                 </ul>
               </div>
+              <div class="font-bold text-xl mb-2">Keywords</div>
+              <div v-for="(count, keyword) in Object.entries(keywords).slice(0, limit)" :key="keyword">
+                {{ keyword }}: {{ count }}
+              </div>
+              <button @click="showMore">Show more</button>
             </section>
       </section>
     </section>
