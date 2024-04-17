94bcf884cb28ae1d9f75ea6fc7eff924ba0067ef
Rami James <ramijames@gmail.com>
2024-04-17 16:16:58 -0400

    Update to nav to use the exported routes

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,5 +1,10 @@
 <script setup>
 import { ref } from "vue";
+
+import color from '@/router/routes-color-tools';
+import css from '@/router/routes-css-tools';
+import text from '@/router/routes-text-tools';
+
 import {
   Dialog,
   DialogPanel,
@@ -23,70 +28,8 @@ import {
   ChevronDownIcon,
 } from "@heroicons/vue/20/solid";
 
-const text = [
-  {
-    name: "Lorem Ipsum",
-    description: "Generate placeholder text",
-    href: "/text-tools/lipsum",
-    icon: DocumentTextIcon,
-  },
-  {
-    name: "Text Analysis",
-    description: "Word counter and keywords analysis tool",
-    href: "/text-tools/word-counter",
-    icon: MagnifyingGlassCircleIcon,
-  },
-];
-
-const css = [
-  {
-    name: "Static Type Scale",
-    description: "Create a static typographic scale",
-    href: "/css-tools/static-type-scale",
-    icon: MusicalNoteIcon,
-  },
-];
-
-const color = [
-  {
-    name: "Palette Generator",
-    description: "Create beautiful color palettes",
-    href: "/color-tools/palette-generator",
-    icon: SwatchIcon,
-  },
-  {
-    name: "Lighten Colors",
-    description: "A set of lighter values",
-    href: "/color-tools/lighten-color",
-    icon: SwatchIcon,
-  },
-  {
-    name: "Darken Colors",
-    description: "A set of darker values",
-    href: "/color-tools/darken-color",
-    icon: SwatchIcon,
-  },
-];
-
-const categories = [
-  {
-    name: "Color Tools",
-    items: color,
-    link: "/color-tools"
-  },
-  {
-    name: "Text Tools",
-    items: text,
-    link: "/text-tools"
-  },
-  {
-    name: "CSS Tools",
-    items: css,
-    link: "/css-tools"
-  }
-];
-
 const mobileMenuOpen = ref(false);
+
 </script>
 
 <template>
@@ -116,13 +59,9 @@ const mobileMenuOpen = ref(false);
           <transition enter-active-class="transition ease-out duration-200" enter-from-class="opacity-0 translate-y-1" enter-to-class="opacity-100 translate-y-0" leave-active-class="transition ease-in duration-150" leave-from-class="opacity-100 translate-y-0" leave-to-class="opacity-0 translate-y-1">
             <PopoverPanel class="absolute -right-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5">
               <div class="p-4">
-                <div v-for="item in color" :key="item.name" class="group relative flex gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50">
-                  <div class="mt-1 flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
-                    <component :is="item.icon" class="h-6 w-6 text-gray-600 group-hover:text-indigo-600" aria-hidden="true"/>
-                  </div>
+                <div v-for="item in color" :key="item.name" class="group relative flex gap-x-6 rounded-lg p-2 text-sm leading-6 hover:bg-gray-50">
                   <div class="flex-auto">
                     <a :href="item.href" class="block font-semibold text-gray-900">{{ item.name }}<span class="absolute inset-0" /></a>
-                    <p class="mt-1 text-gray-600">{{ item.description }}</p>
                   </div>
                 </div>
               </div>
@@ -145,13 +84,9 @@ const mobileMenuOpen = ref(false);
           <transition enter-active-class="transition ease-out duration-200" enter-from-class="opacity-0 translate-y-1" enter-to-class="opacity-100 translate-y-0" leave-active-class="transition ease-in duration-150" leave-from-class="opacity-100 translate-y-0" leave-to-class="opacity-0 translate-y-1">
             <PopoverPanel class="absolute -right-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5">
               <div class="p-4">
-                <div v-for="item in text" :key="item.name" class="group relative flex gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50">
-                  <div class="mt-1 flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
-                    <component :is="item.icon" class="h-6 w-6 text-gray-600 group-hover:text-indigo-600" aria-hidden="true"/>
-                  </div>
+                <div v-for="item in text" :key="item.name" class="group relative flex gap-x-6 rounded-lg p-2 text-sm leading-6 hover:bg-gray-50">
                   <div class="flex-auto">
                     <a :href="item.href" class="block font-semibold text-gray-900">{{ item.name }}<span class="absolute inset-0" /></a>
-                    <p class="mt-1 text-gray-600">{{ item.description }}</p>
                   </div>
                 </div>
               </div>
@@ -174,13 +109,9 @@ const mobileMenuOpen = ref(false);
           <transition enter-active-class="transition ease-out duration-200" enter-from-class="opacity-0 translate-y-1" enter-to-class="opacity-100 translate-y-0" leave-active-class="transition ease-in duration-150" leave-from-class="opacity-100 translate-y-0" leave-to-class="opacity-0 translate-y-1">
             <PopoverPanel class="absolute -right-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5">
               <div class="p-4">
-                <div v-for="item in css" :key="item.name" class="group relative flex gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50">
-                  <div class="mt-1 flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
-                    <component :is="item.icon" class="h-6 w-6 text-gray-600 group-hover:text-indigo-600" aria-hidden="true"/>
-                  </div>
+                <div v-for="item in css" :key="item.name" class="group relative flex gap-x-6 rounded-lg p-2 text-sm leading-6 hover:bg-gray-50">
                   <div class="flex-auto">
                     <a :href="item.href" class="block font-semibold text-gray-900">{{ item.name }}<span class="absolute inset-0" /></a>
-                    <p class="mt-1 text-gray-600">{{ item.description }}</p>
                   </div>
                 </div>
               </div>
