7e3b9d8098bc1609c7cd14faad77b2e9f28ae8e2
Rami James <ramijames@gmail.com>
2024-04-20 17:12:11 -0400

    Viewtype mobile fixes, confirmed working in Safari on iOS

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,17 @@
   <!-- <Header /> -->
   <section :class="theme" class="bg-white dark:bg-slate-900">
     <header class="w-full p-4 border-b dark:border-slate-100/10">
-      <nav class="max-w-screen-2xl w-full mx-auto flex flex-row items-center justify-between px-0 lg:px-4" aria-label="Global" >
-        <span class="text-sm font-semibold text-slate-500 dark:text-white">Welcome</span>
+      <nav class="max-w-screen-2xl w-full mx-auto flex flex-row items-center justify-between lg:px-4" aria-label="Global" >
+        <section class="flex flex-row justify-center">
           <router-link to="/" class="flex flex-row gap-2 justify-center items-center">
             <img class="h-12 w-auto" src="/crisp-logo-white.svg" alt="Crisp Tools"/>
           </router-link>
-          <button @click="toggleTheme()" class="flex flex-row gap-2 px-2 py-1 text-white rounded text-xs bg-blue-500 dark:bg-slate-800 uppercase">
-            <SunIcon class="h-4 w-auto" v-if="theme === 'light'" />
-            <MoonIcon class="h-4 w-auto" v-if="theme === 'dark'" /> <span class="hidden">{{ theme }}</span>
-          </button>
+          <span class="self-center ml-4 text-sm font-semibold text-slate-700 dark:text-slate-500">Welcome</span>
+        </section>
+        <button @click="toggleTheme()" class="flex flex-row px-2 py-1 text-white rounded text-xs bg-blue-500 dark:bg-slate-800 uppercase">
+          <SunIcon class="h-4 w-auto" v-if="theme === 'light'" />
+          <MoonIcon class="h-4 w-auto" v-if="theme === 'dark'" /> <span class="hidden">{{ theme }}</span>
+        </button>
       </nav>
     </header>
     <section class="flex flex-col lg:flex-row max-w-screen-2xl mx-auto w-full">
